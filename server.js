1080ad2082657805ea57403a12956b13496a1ec3
Aryan Pandeya <64304360+aryanpandeya@users.noreply.github.com>
2023-07-08 16:37:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', (req,res) => {
+    var wt = Number(req.body.weight);
+    var ht = Number(req.body.height);
+    var bmi = wt/(ht*ht);
+    bmi = +bmi.toFixed(2);
+
+    res.send("Your BMI is " + bmi);
+})
+
+
+app.listen(port, () => {
+  console.log(`BMI Calculator app listening on port ${port}`)
+})
